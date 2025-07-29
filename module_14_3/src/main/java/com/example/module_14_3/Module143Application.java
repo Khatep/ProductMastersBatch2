8d46c3aae91a85b8d49c7ba7e3f0c4d9775ee57f
Khatep <aserty664qer@gmail.com>
2025-07-30 00:10:27 +0500

    HW 14 Hard :: New Project

---
+++ module_14_3/src/main/java/com/example/module_14_3/Module143Application.java
@@ -0,0 +1,13 @@
+package com.example.module_14_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module143Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module143Application.class, args);
+    }
+
+}
